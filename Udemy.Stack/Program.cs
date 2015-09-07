9616739632dddc9e4d896b83289a64ee456c006b
jayason@gmail.com <tvj$on19>
2015-09-07 11:09:57 +0530

    Added stack exercise

---
+++ Udemy.Stack/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Udemy.Stack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var stack = new Stack();
+                stack.Push(1);
+                stack.Push(2);
+                //stack.Push(null);
+
+                Console.WriteLine(stack.Pop());
+                Console.WriteLine(stack.Pop());
+                Console.WriteLine(stack.Pop());
+            }
+            catch (InvalidOperationException ex)
+            {
+                Console.WriteLine("inavlid opeartion");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Udemy.Stack/Stack.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Udemy.Stack
+{
+    public class Stack
+    {
+        private readonly List<object> stackObject;
+
+        public Stack()
+        {
+            stackObject = new List<object>();
+        }
+        public void Push(object obj)
+        {
+            if (obj != null)
+            {
+                stackObject.Add(obj);
+            }
+            else
+            {
+                throw new InvalidOperationException();
+            }
+        }
+
+        public object Pop()
+        {
+            var itemCount = stackObject.Count;
+            if (itemCount == 0)
+            {
+                throw new InvalidOperationException();
+            }
+            else
+            {
+                var lastItem = stackObject[itemCount - 1];
+                stackObject.RemoveAt(itemCount - 1);
+                return lastItem;
+            }
+        }
+
+        public void Clear()
+        {
+            stackObject.Clear();
+        }
+    }
+}
