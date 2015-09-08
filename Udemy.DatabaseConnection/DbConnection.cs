f33743498baf83561389e86ed468b93edf287cc0
jayason@gmail.com <tvj$on19>
2015-09-08 18:40:29 +0530

    Connection & command exercise

---
+++ Udemy.DatabaseConnection/DbConnection.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Udemy.DatabaseConnection
+{
+    public abstract class DbConnection
+    {
+        public string ConnectionString { get; private set; }
+
+        public TimeSpan Timeout { get; private set; }
+
+        public DbConnection(string connectionString)
+        {
+            if (!string.IsNullOrEmpty(connectionString))
+            {
+                ConnectionString = connectionString;
+            }
+            else
+            {
+                throw new ArgumentNullException();
+            }
+        }
+
+        public abstract void Open();
+
+        public abstract void Close();
+    }
+}
---
+++ Udemy.DatabaseConnection/SqlConnection.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Udemy.DatabaseConnection
+{
+    public class SqlConnection : DbConnection
+    {
+        public SqlConnection(string connectionString)
+            : base(connectionString)
+        {
+
+        }
+        public override void Open()
+        {
+            Console.WriteLine("Sql connection opened");
+        }
+
+        public override void Close()
+        {
+            Console.WriteLine("Sql connection closed");
+        }
+    }
+}
